24f68804e448f0f41c68bfd6828de6a3dd50bc58
lipeapp <serverlipe@gmail.com>
2021-04-07 15:16:08 +0500

    updated

--- src/sample/api/Key.java
+++ src/sample/api/Key.java
@@ -1,6 +1,6 @@
 package sample.api;
 
 public interface Key {
-    //https://openweathermap.org/api dan klyuch olib hoylashingiz kerak
+    //https://openweathermap.org/api dan klyuch olib joylashingiz kerak
     String weather_key = "";
 }
